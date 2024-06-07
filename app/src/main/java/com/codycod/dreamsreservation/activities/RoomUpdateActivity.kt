362d7cbe80e8ffbb6a5a40ee4f4bc44509b30a37
JeancaDeve <khevinelpro@gmail.com>
2024-06-06 23:01:22 -0500

    Se empezo el proyecto android

---
+++ app/src/main/java/com/codycod/dreamsreservation/activities/RoomUpdateActivity.kt
@@ -0,0 +1,13 @@
+package com.codycod.dreamsreservation.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codycod.dreamsreservation.R
+
+class RoomUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_room_admin)
+    }
+}
